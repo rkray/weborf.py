1a743d319f9669b063dfbc4d004f836f5fac0237
René Kray <rene.kray@billpay.de>
2017-03-20 15:14:04 +0100

    add new code for the network device and address detection

--- weborf.py
+++ weborf.py
@@ -10,7 +10,10 @@ from socket import socket, AF_INET, SOCK_DGRAM, inet_ntoa
 from fcntl import ioctl
 from struct import pack
 
-from os import listdir
+import re
+
+from os import listdir,popen
+from sys import platform
 
 from optparse import OptionParser
 #from re import match
@@ -18,10 +21,12 @@ from optparse import OptionParser
 class WebOrf:
     def __init__(self):
         # defiune defaults here
+    
         self.conf=dict(
             verbose=True,
             port=8000,
-            ip=WebOrf.get_ip_address('eth0') # '192.168.0.110'
+            interface=WebOrf.get_interface(),
+            ip=None
         )
 
     def run(self):
@@ -29,6 +34,10 @@ class WebOrf:
         HandlerClass = SimpleHTTPRequestHandler
         ServerClass  = HTTPServer
         Protocol     = "HTTP/1.0"
+
+        # if the is no ip address specified, I will try to detect the right one
+        if self.conf["ip"]==None:
+            self.conf["ip"]=WebOrf.get_ip_address(self.conf["interface"])
 
         server_address=(
             self.conf["ip"],
@@ -55,6 +64,14 @@ class WebOrf:
             dest="port",default=self.conf["port"],
             help="set listen port number", metavar="PORT")
         parser.add_option(
+            "-i", "--interface",
+            dest="interface",default=self.conf["interface"],
+            help="set the network interface", metavar="INTERVACE")
+        parser.add_option(
+            "-a", "--address",
+            dest="ip",default=self.conf["ip"],
+            help="set the network ip address", metavar="ADDRESS")
+        parser.add_option(
             "-q", "--quiet",
             action="store_false", dest="verbose", default=True,
             help="don't print status messages to stdout")
@@ -66,14 +83,61 @@ class WebOrf:
     @classmethod
     # find the IP address for eth0
     def get_ip_address(cls,ifname):
-        s = socket(AF_INET, SOCK_DGRAM)
-        return inet_ntoa(
-            ioctl(
-                s.fileno(),
-                0x8915, # SIOCGIFADDR
-                pack('256s', ifname[:15])
-            )[20:24]
-        )
+        ## I was not able to implement this function on Mac OS
+        # s = socket(AF_INET, SOCK_DGRAM)
+        # return inet_ntoa(
+        #     ioctl(
+        #         s.fileno(),
+        #         0x8915, # SIOCGIFADDR
+        #         pack('256s', ifname[:15])
+        #     )[20:24]
+        # )
+
+        my_ip_address=""
+
+        if platform in ["linux2","lunux3"]:
+            infile=popen("/sbin/ifconfig "+ifname).read()
+            inarray=infile.split("\n")
+            for line in inarray:
+                print(line)
+
+                match=re.match(r".*inet addr:(\d+\.\d+\.\d+\.\d+)",line)
+                if match:
+                    return match.group(1)
+            raise "There is no ip for the interface {iface} configured".format(iface=ifname)
+
+
+        # MacOSX detection
+        elif platform in ["darwin"]:
+            infile=popen("/sbin/ifconfig "+ifname).read()
+            inarray=infile.split("\n")
+            for line in inarray:
+                print(line)
+
+                match=re.match(r".*inet addr:(\d+\.\d+\.\d+\.\d+)",line)
+                if match:
+                    return match.group(1)
+            raise "There is no ip for the interface {iface} configured".format(iface=ifname)
+        else:
+            raise "unsuportet platform"
+
+    @classmethod
+    # get default interface
+    def get_interface(cls):
+        if platform in ["linux2","lunux3"]:
+            infile=popen("netstat -rn").read()
+            inarray=infile.split("\n")
+            for line in inarray:
+                match=re.match(r"^0\.0\.0\.0 .* (.*)$",line)
+                if match:
+                    return match.group(1)
+            raise "No default gateway found"
+
+        elif platform in ["darwin"]:
+            print("MAC OS detected")
+
+        else:
+            raise "unsuportet platform"
 
 # END of WebOrf class
 
